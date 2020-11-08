d8915ade5b5d0f4351efce2f8476aaebeb068493
DangKhoa <iunc129@gmail.com>
2020-11-08 16:25:02 +0700

    ver3

--- models/LopHoc.js
+++ models/LopHoc.js
@@ -1,11 +1,7 @@
 var LopHoc = function(){
     this.danhSachSinhVien = [];
-<<<<<<< HEAD
     // User A
     this.mota = 'Đây là lớp frontend 54 đang học git!!!!';
-=======
-    this.tenLopHoc ='fe54';
->>>>>>> 8a9311e56b9aaf36f2273305c0fd33d5d2458bee
     this.layDanhSachSinhVien = function() {
         return this.danhSachSinhVien;
     }
