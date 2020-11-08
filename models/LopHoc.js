ea94c412ebfa4b0e4e5536e34e9025b41c347291
quachphuong272 <hannychan123@gmail.com>
2020-11-08 15:46:55 +0700

    UserA_commit LopHoc.js

--- models/LopHoc.js
+++ models/LopHoc.js
@@ -1,5 +1,7 @@
 var LopHoc = function(){
     this.danhSachSinhVien = [];
+    // User A
+    this.mota = 'Đây là lớp frontend 54 đang học git!!!!';
     this.layDanhSachSinhVien = function() {
         return this.danhSachSinhVien;
     }
